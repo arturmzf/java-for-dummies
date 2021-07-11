f1859162577b19c206e23be1ea5328d6d6948bbb
Artur Muzafarov <art.mzf@gmail.com>
2021-07-11 21:31:47 +0300

    Listing0100 PayBerry Class creating

---
+++ src/part1/chapter01/Listing0100PayBerry.java
@@ -0,0 +1,15 @@
+package part1.chapter01;
+
+/*
+* Чтобы ручки вспомнили, что это вообще такое...
+* */
+
+public class Listing0100PayBerry {
+    public static void main(String[] args){
+        double checkAmount = 1257.63;
+        System.out.print("Оплатить заказ Барри Берда, ");
+        System.out.print("сумма: ");
+        System.out.print(checkAmount);
+        System.out.println(" рублей.");
+    }
+}
